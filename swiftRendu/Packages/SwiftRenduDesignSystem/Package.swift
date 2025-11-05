6b70962c7b668796d927a894bb6a759db61fb805
DAGUET Romain <rd253570@djjj760vvw.edu.atlas.local>
2025-11-05 10:10:28 +0100

    :fire: defined project architecture

---
+++ swiftRendu/Packages/SwiftRenduDesignSystem/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftRenduDesignSystem",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftRenduDesignSystem",
+            targets: ["SwiftRenduDesignSystem"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftRenduDesignSystem"),
+
+    ]
+)
