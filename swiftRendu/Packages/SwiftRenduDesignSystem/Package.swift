aeb526e671573b8da8b2f61d6c70c9e0de62ab7d
DELMAS Olivier <od236147@dx3ntlpgwp.edu.atlas.local>
2025-11-05 10:23:21 +0100

    :fire: packaged architecture implemented

--- swiftRendu/Packages/SwiftRenduDesignSystem/Package.swift
+++ swiftRendu/Packages/SwiftRenduDesignSystem/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftRenduDesignSystem",
+    platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +16,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftRenduDesignSystem"),
-
+            name: "SwiftRenduDesignSystem",
+            resources: [
+                .process("Ressources")
+            ]
+        )
     ]
 )
