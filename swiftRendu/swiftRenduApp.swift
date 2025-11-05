cf71ae6a4e14a29a3ec7cc419248c5fd0d9514f0
DAGUET Romain <rd253570@djjj760vvw.edu.atlas.local>
2025-11-05 09:34:24 +0100

    :wave: init project

---
+++ swiftRendu/swiftRenduApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftRenduApp.swift
+//  swiftRendu
+//
+//  Created by DAGUET Romain on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftRenduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
